f575cb94b44fd39dc156a8805bafa0ea6d11dc3f
Hampus833 <hampuse266@gmail.com>
2024-11-28 12:00:28 +0100

    added new functions
    check if body is empty check if content type is ok and a function to send responses.

--- api/functions.js
+++ api/functions.js
@@ -4,13 +4,15 @@ export async function getFile(file) {
     return JSON.parse(await Deno.readTextFile(file));
 }
 
-// export async function wrongMethod(method, allowedMethod) {
-//     if (method !== allowedMethod) {
-//         return new Response ("Only " + allowedMethod + " is allowed", {status: 405});
-//     }
-// }
+export async function sendResponse(text, status) {
+     return new Response(text, {status: status});
+}
 
-// export async function handler(req){
-//     const requestMethod = req.method;
-//     return requestMethod;
-// }
+export async function checkContentType(currentType, correctType) {
+    return currentType === correctType;
+}
+
+export async function checkBody(body) {
+    return Object.keys(body).length === 0;
+}
+
--- api/login.js
+++ api/login.js
@@ -4,24 +4,30 @@ import * as func from "./functions.js";
 
 async function handler(req){
 
-    const requestMethod = req.method;
     const allowedMethod = "POST";
+    const requestMethod = req.method;
 
-    //kolla så det är application/json
     //kolla så bodyn inte är tom
-    //kolla så att username och password finns med
-    //kolla så att de stämmer överens
+    //skicka tillbaka användaren
     
-            // gör om till en funktion och lääg i functions.js
-            // if (!currentUser) {
-            //     return new Response("Body can't be empty", {status: 400});
-            // };
-
     if (requestMethod === allowedMethod) {
         console.log(allowedMethod);
+        
+        const contentType = req.headers.get("Content-Type");
+        const isValidMethod = await func.checkContentType(contentType, "application/json");
+        
+        if (!isValidMethod) {
+            return await func.sendResponse("Incorrect Content-Type, only JSON allowed", 400);
+        }
 
         const currentUser = await req.json();
         console.log(currentUser);
+        const currentUserEmpty = await func.checkBody(currentUser);
+        console.log(currentUserEmpty);
+
+        if (currentUserEmpty) {
+            return await func.sendResponse("Body can't be empty", 400);
+        }
 
         if (!currentUser.username) {
             return new Response("Body must include a username", {status: 400});
@@ -30,37 +36,27 @@ async function handler(req){
         if (!currentUser.password) {
             return new Response("Body must include a password", {status: 400});
         }
- 
+
         const username = currentUser.username;
         const password = currentUser.password;
         
         const allUsers = await func.getFile("./database/users.json");
-        console.log(allUsers);
 
         for (const user of allUsers) {
             console.log(user);
 
             if (username !== user.username && (password !== user.password)) {
-                return new Response("User not found", {status: 404});
+                return await func.sendResponse("User not found", 400);
 
             } else if (username === user.username && (password !== user.password)) {
-                return new Response("Incorrect password", {status: 400});
+                return await func.sendResponse("Incorrect password,", 400);
 
-            } else if (username !== username.username && (password === user.password)) {
-                return new Response("Incorrect username", {status: 400});
+            } else if (username !== user.username && (password === user.password)) {
+                return await func.sendResponse("Incorrect username", 400);
             }
         }
-        
-   
 
-
-
-
-
-
-
-
-        return new Response("Correct method used!", { status: 200});
+        return new Response("Logged in", { status: 200});
     } else {
         return new Response ("Only POST is allowed", {status: 405});
     }
