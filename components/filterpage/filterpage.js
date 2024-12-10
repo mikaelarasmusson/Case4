959eeb56ffba242cb5a42dc4e7d1239191385bb3
mikaelarasmusson <143449047+mikaelarasmusson@users.noreply.github.com>
2024-12-10 22:13:54 +0100

    updated pics in database, added code to js and css

--- components/filterpage/filterpage.js
+++ components/filterpage/filterpage.js
@@ -34,6 +34,8 @@ function renderProfileWithBackArrow(parentId) {
     const profileContent = document.createElement("div");
     profileContent.id = "profileContentWithBackArrow";
     profileContent.classList.add("profile");
+
+    let userInfo = State.get("users");
     
     profileContent.innerHTML = `
       <button id="backArrow">
@@ -41,14 +43,20 @@ function renderProfileWithBackArrow(parentId) {
             <path d="M9 19.6567L0 10L9 0.343346L11.1 2.67704L5.775 8.39056H24V11.6094H5.775L11.1 17.323L9 19.6567Z" fill="white"/>
         </svg>
       </button>
-      <svg id="starIcon" xmlns="http://www.w3.org/2000/svg" width="19" height="20" viewBox="0 0 19 20" fill="none">
+      <div id="profileCont">
+        <svg id="starIcon" xmlns="http://www.w3.org/2000/svg" width="19" height="20" viewBox="0 0 19 20" fill="none">
           <path d="M18.9854 8.10816C19.0803 7.6103 18.7006 7.01288 18.2259 7.01288L12.8151 6.21631L10.347 1.03863C10.2521 0.839491 10.1572 0.73992 9.96731 0.640349C9.49268 0.341637 8.92312 0.540778 8.63834 1.03863L6.26517 6.21631L0.854341 7.01288C0.569561 7.01288 0.379707 7.11245 0.28478 7.31159C-0.0949268 7.70987 -0.0949268 8.3073 0.28478 8.70558L4.17678 12.6884L3.22751 18.3639C3.22751 18.5631 3.22751 18.7622 3.32244 18.9614C3.60722 19.4592 4.17678 19.6584 4.65141 19.3597L9.49268 16.6712L14.3339 19.3597C14.4289 19.4592 14.6187 19.4592 14.8086 19.4592C14.9035 19.4592 14.9035 19.4592 14.9984 19.4592C15.4731 19.3597 15.8528 18.8618 15.7578 18.2644L14.8086 12.5888L18.7006 8.60601C18.8904 8.50644 18.9854 8.3073 18.9854 8.10816Z" fill="#FFD07A"/>
-      </svg>
-      <p id="userPoints">345</p>
-      <img id="profilePic">
+        </svg>
+        <p id="userPoints">${userInfo.score}</p>
+        <img id="profilePic" src="${userInfo.profilePic}">
+      </div>
     `;
-  
+
     profileContainer.appendChild(profileContent);
+
+    document.getElementById("backArrow").addEventListener("click", () => {
+        renderLandingpageContainer("wrapper");
+    });
 }
 
 function renderSearchbar(parentId) {
@@ -94,6 +102,14 @@ function renderFilterContents (parentId) {
     buttonFilms.classList.add("filterpageButton");
     buttonFilms.textContent = "Films";
     buttonContainer.appendChild(buttonFilms);
+
+    buttonFilms.addEventListener("click", () => {
+        const filmsandSeriesContainer = document.getElementById("filmsandSeriesBoxesContainer");
+        filmsandSeriesContainer.innerHTML = "";
+
+        const onlyFilms = State.get("films");
+        renderFilmsandSeriesBoxes(onlyFilms, filmsandSeriesContainer);
+    });
     
     const buttonSeries = document.createElement("button");
     buttonSeries.id = "buttonSeries";    
--- components/landingpage/landingpage.js
+++ components/landingpage/landingpage.js
@@ -31,13 +31,15 @@ function renderProfileLandingpage(parentId) {
   const profileContent = document.createElement("div");
   profileContent.id = "profileContentLandingpage";
   profileContent.classList.add("profile");
+
+  let userInfo = State.get("users");
   
   profileContent.innerHTML = `
     <svg id="starIcon" xmlns="http://www.w3.org/2000/svg" width="19" height="20" viewBox="0 0 19 20" fill="none">
         <path d="M18.9854 8.10816C19.0803 7.6103 18.7006 7.01288 18.2259 7.01288L12.8151 6.21631L10.347 1.03863C10.2521 0.839491 10.1572 0.73992 9.96731 0.640349C9.49268 0.341637 8.92312 0.540778 8.63834 1.03863L6.26517 6.21631L0.854341 7.01288C0.569561 7.01288 0.379707 7.11245 0.28478 7.31159C-0.0949268 7.70987 -0.0949268 8.3073 0.28478 8.70558L4.17678 12.6884L3.22751 18.3639C3.22751 18.5631 3.22751 18.7622 3.32244 18.9614C3.60722 19.4592 4.17678 19.6584 4.65141 19.3597L9.49268 16.6712L14.3339 19.3597C14.4289 19.4592 14.6187 19.4592 14.8086 19.4592C14.9035 19.4592 14.9035 19.4592 14.9984 19.4592C15.4731 19.3597 15.8528 18.8618 15.7578 18.2644L14.8086 12.5888L18.7006 8.60601C18.8904 8.50644 18.9854 8.3073 18.9854 8.10816Z" fill="#FFD07A"/>
     </svg>
-    <p id="userPoints">345</p>
-    <img id="profilePic">
+    <p id="userPoints">${userInfo.score}</p>
+    <img id="profilePic" src="${userInfo.profilePic}">
   `;
 
   profileContainer.append(profileContent);
@@ -57,12 +59,11 @@ function renderLandingpageContent(parentId) {
   contentElement.classList.add("landingpage");
 
   // Set up the content element's HTML
-  const userPoints = "";
-  const username = "";
+  const username = State.get("users");
   contentElement.innerHTML = `
     <div id="welcomeContainer">    
       <h1 class="landingpageTitle">Make a choice</h1>
-      <p class="playText">Welcome back ${username}, how would you like to play?</p>
+      <p class="playText">Welcome back ${username.username}, how would you like to play?</p>
     </div>
     <div id="buttonContainer">
       <button id="buttonSinglePlayer" class="landingpageButton">Single Player</button>
