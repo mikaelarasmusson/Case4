7484ce6c9b5de38bfe955d125074a7c327782804
mikaelarasmusson <143449047+mikaelarasmusson@users.noreply.github.com>
2024-12-13 12:23:40 +0100

    fixed categories button

--- components/filterpage/filterpage.js
+++ components/filterpage/filterpage.js
@@ -124,49 +124,6 @@ function renderFilterContents(parentId) {
     parent.appendChild(buttonContainer);
 }
 
-
-function renderFilterDropDownPopUp(parentId) {
-    const parent = document.getElementById(parentId);
-
-    // Kontrollera om popupen redan finns för att förhindra duplicering
-    if (document.getElementById("categoriesDropdown")) {
-        return;
-    }
-
-    const categoriesDropdown = document.createElement("div");
-    categoriesDropdown.id = "categoriesDropdown";
-    categoriesDropdown.innerHTML = `
-        <button id="exitDropdown">
-            <svg xmlns="http://www.w3.org/2000/svg" width="19" height="19" viewBox="0 0 19 19" fill="none">
-                <path d="M11.1732 9.49899L18.649 2.03423C18.8724 1.81075 18.998 1.50766 18.998 1.19162C18.998 0.875585 18.8724 0.572491 18.649 0.349018C18.4255 0.125546 18.1225 0 17.8065 0C17.4905 0 17.1874 0.125546 16.964 0.349018L9.5 7.82565L2.03603 0.349018C1.81259 0.125546 1.50952 2.80593e-07 1.19352 2.82947e-07C0.877516 2.85302e-07 0.574455 0.125546 0.351006 0.349018C0.127557 0.572491 0.00202541 0.875585 0.0020254 1.19162C0.0020254 1.50766 0.127557 1.81075 0.351006 2.03423L7.82684 9.49899L0.351006 16.9637C0.239784 17.0741 0.151505 17.2053 0.0912611 17.35C0.031017 17.4946 0 17.6497 0 17.8064C0 17.963 0.031017 18.1181 0.0912611 18.2628C0.151505 18.4074 0.239784 18.5386 0.351006 18.649C0.46132 18.7602 0.592563 18.8485 0.737166 18.9087C0.881769 18.969 1.03687 19 1.19352 19C1.35017 19 1.50527 18.969 1.64987 18.9087C1.79448 18.8485 1.92572 18.7602 2.03603 18.649L9.5 11.1723L16.964 18.649C17.0743 18.7602 17.2055 18.8485 17.3501 18.9087C17.4947 18.969 17.6498 19 17.8065 19C17.9631 19 18.1182 18.969 18.2628 18.9087C18.4074 18.8485 18.5387 18.7602 18.649 18.649C18.7602 18.5386 18.8485 18.4074 18.9087 18.2628C18.969 18.1181 19 17.963 19 17.8064C19 17.6497 18.969 17.4946 18.9087 17.35C18.8485 17.2053 18.7602 17.0741 18.649 16.9637L11.1732 9.49899Z" fill="white"/>
-            </svg>
-        </button>
-        <div id="linksContainer">
-            <a href="">Action</a>
-            <a href="">Adventure</a>
-            <a href="">Biography</a>
-            <a href="">Comedy</a>
-            <a href="">Crime</a>
-            <a href="">Drama</a>
-            <a href="">Fantasy</a>
-            <a href="">History</a>
-            <a href="">Horror</a>
-            <a href="">Mystery</a>
-            <a href="">Romance</a>
-            <a href="">Sci-Fi</a>
-            <a href="">Sport</a>
-            <a href="">Thriller</a> 
-        </div>   
-        `;
-
-    // Lägg till händelse för att stänga popupen
-    categoriesDropdown.querySelector("#exitDropdown").addEventListener("click", () => {
-        categoriesDropdown.remove();
-    });
-
-    parent.appendChild(categoriesDropdown);
-}
-
 function renderFilmsandSeriesBoxesContainer(parentId) {
     const parent = document.getElementById(parentId);
     const container = document.createElement("div");
@@ -175,8 +132,8 @@ function renderFilmsandSeriesBoxesContainer(parentId) {
     renderFilmsandSeriesBoxes(container);
 }
 
-function renderFilmsandSeriesBoxes(parentDom) {
-    const allMedia = [...State.get("films"), ...State.get("series")];
+function renderFilmsandSeriesBoxes(parentDom, mediaList = null) {
+    const allMedia = mediaList || [...State.get("films"), ...State.get("series")];
     const quizFilms = State.get("quizfilms");
     const quizSeries = State.get("quizseries");
 
@@ -224,6 +181,75 @@ function renderFilmsandSeriesBoxes(parentDom) {
             renderStartQuizPopup(parentDom.id, media.id, mediaType);
         });
     }
+}
+
+function filterAndRenderMediaByGenre(genre) {
+    const allMedia = [...State.get("films"), ...State.get("series")];
+    const filteredMedia = allMedia.filter((media) => {
+        return media.genre && media.genre.includes(genre);
+    });
+    console.log(filteredMedia);
+    
+    const container = document.getElementById("filmsandSeriesBoxesContainer");
+    container.innerHTML = ""; // Rensa innehåll i kontaineren
+
+    renderFilmsandSeriesBoxes(container, filteredMedia);
+}
+
+function renderFilterDropDownPopUp(parentId) {
+    const parent = document.getElementById(parentId);
+
+    // Kontrollera om popupen redan finns för att förhindra duplicering
+    if (document.getElementById("categoriesDropdown")) {
+        return;
+    }
+
+    const categoriesDropdown = document.createElement("div");
+    categoriesDropdown.id = "categoriesDropdown";
+    categoriesDropdown.innerHTML = `
+        <button id="exitDropdown">
+            <svg xmlns="http://www.w3.org/2000/svg" width="19" height="19" viewBox="0 0 19 19" fill="none">
+                <path d="M11.1732 9.49899L18.649 2.03423C18.8724 1.81075 18.998 1.50766 18.998 1.19162C18.998 0.875585 18.8724 0.572491 18.649 0.349018C18.4255 0.125546 18.1225 0 17.8065 0C17.4905 0 17.1874 0.125546 16.964 0.349018L9.5 7.82565L2.03603 0.349018C1.81259 0.125546 1.50952 2.80593e-07 1.19352 2.82947e-07C0.877516 2.85302e-07 0.574455 0.125546 0.351006 0.349018C0.127557 0.572491 0.00202541 0.875585 0.0020254 1.19162C0.0020254 1.50766 0.127557 1.81075 0.351006 2.03423L7.82684 9.49899L0.351006 16.9637C0.239784 17.0741 0.151505 17.2053 0.0912611 17.35C0.031017 17.4946 0 17.6497 0 17.8064C0 17.963 0.031017 18.1181 0.0912611 18.2628C0.151505 18.4074 0.239784 18.5386 0.351006 18.649C0.46132 18.7602 0.592563 18.8485 0.737166 18.9087C0.881769 18.969 1.03687 19 1.19352 19C1.35017 19 1.50527 18.969 1.64987 18.9087C1.79448 18.8485 1.92572 18.7602 2.03603 18.649L9.5 11.1723L16.964 18.649C17.0743 18.7602 17.2055 18.8485 17.3501 18.9087C17.4947 18.969 17.6498 19 17.8065 19C17.9631 19 18.1182 18.969 18.2628 18.9087C18.4074 18.8485 18.5387 18.7602 18.649 18.649C18.7602 18.5386 18.8485 18.4074 18.9087 18.2628C18.969 18.1181 19 17.963 19 17.8064C19 17.6497 18.969 17.4946 18.9087 17.35C18.8485 17.2053 18.7602 17.0741 18.649 16.9637L11.1732 9.49899Z" fill="white"/>
+            </svg>
+        </button>
+        <div id="linksContainer">
+            <a href="#" class="categoryLink">Action</a>
+            <a href="#" class="categoryLink">Adventure</a>
+            <a href="#" class="categoryLink">Biography</a>
+            <a href="#" class="categoryLink">Comedy</a>
+            <a href="#" class="categoryLink">Crime</a>
+            <a href="#" class="categoryLink">Drama</a>
+            <a href="#" class="categoryLink">Fantasy</a>
+            <a href="#" class="categoryLink">History</a>
+            <a href="#" class="categoryLink">Horror</a>
+            <a href="#" class="categoryLink">Mystery</a>
+            <a href="#" class="categoryLink">Romance</a>
+            <a href="#" class="categoryLink">Sci-Fi</a>
+            <a href="#" class="categoryLink">Sport</a>
+            <a href="#" class="categoryLink">Thriller</a> 
+        </div>   
+        `;
+
+    // Lägg till händelse för att stänga popupen
+    categoriesDropdown.querySelector("#exitDropdown").addEventListener("click", () => {
+        categoriesDropdown.remove();
+    });
+
+    parent.appendChild(categoriesDropdown);
+
+    const categoryLinks = document.querySelectorAll(".categoryLink");
+    categoryLinks.forEach((link) => {
+        link.addEventListener("click", (e) => {
+            e.preventDefault();
+
+            categoriesDropdown.remove();
+
+            const selectedCategory = link.textContent.trim();
+
+            filterAndRenderMediaByGenre(selectedCategory);
+        });
+    });
+    console.log(categoryLinks);
 }
 
 // Lägg till popup för starta quiz
